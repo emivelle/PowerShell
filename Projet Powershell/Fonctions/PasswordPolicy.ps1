4b821feb715c54185daede9140ea76da7974572a
JOJOTTO <115787204+JOJOTTO@users.noreply.github.com>
2025-03-17 20:18:05 +0100

    Add files via upload

---
+++ Projet Powershell/Fonctions/PasswordPolicy.ps1
@@ -0,0 +1,42 @@
+﻿function PasswordPolicy {
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$DomainNETBIOS
+    )
+
+
+    # Politique pour les utilisateurs normaux
+    Write-Host "Application de la politique de mot de passe pour les utilisateurs normaux..."
+    Set-ADDefaultDomainPasswordPolicy -Identity "$DomainNETBIOS.LOCAL" `
+        -MinPasswordLength 10 `
+        -ComplexityEnabled $true `
+        -PasswordHistoryCount 24 `
+        -ReversibleEncryptionEnabled $false `
+        -LockoutThreshold 5 `
+        -LockoutDuration "00:30:00" `
+        -LockoutObservationWindow "00:30:00" `
+        -MinPasswordAge "1.00:00:00" `
+        -MaxPasswordAge "45.00:00:00"
+
+    # Activation du mode d'approbation administrateur dans l'UAC pour renforcer la sécurité
+    reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v FilterAdministratorToken /t REG_DWORD /d 1 /f
+
+    # Création d'une politique de mot de passe fine pour les administrateurs
+    Write-Host "Application de la politique de mot de passe pour les administrateurs..."
+    New-ADFineGrainedPasswordPolicy -Name "AdminPasswordPolicy" `
+        -Precedence 1 `
+        -MinPasswordLength 16 `
+        -ComplexityEnabled $true `
+        -PasswordHistoryCount 24 `
+        -ReversibleEncryptionEnabled $false `
+        -LockoutThreshold 5 `
+        -LockoutDuration "00:30:00" `
+        -LockoutObservationWindow "00:30:00" `
+        -MinPasswordAge "1.00:00:00" `
+        -MaxPasswordAge "30.00:00:00"
+
+    # Application de la politique fine aux administrateurs
+    Add-ADFineGrainedPasswordPolicySubject -Identity "AdminPasswordPolicy" -Subjects "Admins du domaine"
+    
+    Write-Host "Configuration des politiques de mot de passe terminée."
+}
---
+++ Projet Powershell/Script_Principal.ps1
@@ -0,0 +1,77 @@
+﻿if ((Get-ComputerInfo).CsName -ne "SRV-DC01") {
+    # Si le nom est différent, configurer le nom et redémarrer
+    Rename-Computer -NewName "SRV-DC01" -Restart -Force
+} else {
+    # Si non, passer à la suite
+    Write-Host "Le nom de la machine est déjà SRV-DC01. Aucune action requise."
+}
+
+# Déclaration de l'école unique, du nombre de classes et du domaine
+$SchoolName = "Simone Veil"
+$NumberOfClasses = 17
+$DomainName = $SchoolName.Replace(" ","")
+$Domain = "DC=$DomainName,DC=LOCAL"
+
+# Vérifier si Active Directory est déjà installé
+if (Get-WindowsFeature -Name AD-Domain-Services | Where-Object { $_.Installed }) {
+    Write-Host "Active Directory est déjà installé. Passage à l'étape suivante..." -ForegroundColor Yellow
+} else {
+    # Importer et exécuter l'installation d'AD
+    Import-Module .\Fonctions\Install-AD.ps1
+    Install-AD -DomainNETBIOS $DomainName
+}
+
+
+# Importation du module et création des OUs pour l'école unique
+Import-Module .\Fonctions\Create-SchoolOrganizationalUnits.ps1
+Create-SchoolOrganizationalUnits -SchoolName $SchoolName -Domain $Domain -NumberOfClasses $NumberOfClasses
+
+# Définir le chemin du fichier CSV unique des élèves
+$CSVPath = "Utilisateurs\Eleves\Simone-Veil.csv"
+
+# Vérifier si le fichier existe
+if (-not (Test-Path -Path $CSVPath)) {
+    Write-Warning "Le fichier CSV des élèves n'existe pas : $CSVPath."
+    return
+}
+
+# Traitement du fichier CSV des élèves
+Write-Host "Traitement du fichier : $CSVPath"
+
+try {
+    # Importation du module et création des utilisateurs élèves
+    Import-Module .\Fonctions\Create-Students.ps1
+    Create-Students -CSVPath $CSVPath -Domain $Domain -SchoolName $SchoolName
+} catch {
+    Write-Warning "Erreur lors du traitement du fichier des élèves : $_"
+}
+
+$CSVPath = "Utilisateurs\Enseignants\Enseignants.csv"
+
+# Création des enseignants
+Import-Module .\Fonctions\Create-Teachers.ps1
+Create-Teachers -CSVPath $CSVPath -Domain $Domain -TargetSchool $SchoolName
+
+# Mise en place de politique de mot de passe
+Import-Module .\Fonctions\PasswordPolicy.ps1
+PasswordPolicy -DomainNETBIOS $DomainName
+
+
+
+
+#############################################################################################
+#                                    Application des GPO                                    #
+#############################################################################################
+
+
+# Mise en place de la restrictions d'installation de logiciel pour les eleves
+Import-Module .\Fonctions\GPO_Restrict_Admin_Access.ps1
+GPO_Restrict_Admin_Access -DomainNETBIOS $DomainName
+
+Import-Module .\Fonctions\GPO_Teacher_Logon_Restrict.ps1
+GPO_Teacher_Logon_Restrict -DomainNETBIOS $DomainName
+
+Import-Module .\Fonctions\GPO_Student_Logon_Restrict.ps1
+GPO_Student_Logon_Restrict -DomainNETBIOS $DomainName
+
+
